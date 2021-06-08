7081b2da2583676b584ad12341831a6bd6f433e2
Sean Joyce <joycesean702@gmail.com>
2021-06-08 17:28:00 +0100

    further refined backend and started front end refinment

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const path = require('path')
 const mongoose = require('mongoose');
-const Entry = require('./models/entry')
+const Entry = require('./models/entry');
+const { findById } = require('./models/entry');
+const methodOverride = require('method-override')
+const ejsMate = require('ejs-mate')
 
 mongoose.connect('mongodb://localhost:27017/happiness-journal', {
     useNewUrlParser: true,
@@ -20,18 +23,59 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
+app.engine('ejs', ejsMate)
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
 app.get('/', (req, res) => {
     res.render('home')
 })
 
+
+//all entries
 app.get('/entries', async (req, res) => {
     const entries = await Entry.find({})
     res.render('entries/index', { entries })
 })
 
+//new entry
+app.get('/entries/new', (req, res) => {
+    res.render('entries/new')
+})
+
+app.post('/entries', async (req, res) => {
+    const entry = new Entry(req.body.entry);
+    await entry.save();
+    res.redirect(`/entries/${entry._id}`);
+})
+
+
+//show entry
 app.get('/entries/:id', async (req, res) => {
     const entry = await Entry.findById(req.params.id)
     res.render('entries/show', { entry })
+})
+
+//edit entry
+app.get('/entries/:id/edit', async (req, res) => {
+    const entry = await Entry.findById(req.params.id)
+    res.render('entries/edit', { entry })
+})
+
+
+//Update route
+app.put('/entries/:id', async (req, res) => {
+    const { id } = req.params;
+    const entry = await Entry.findByIdAndUpdate(id, { ...req.body.entry })
+    res.redirect(`/entries/${entry._id}`);
+})
+
+
+//delete route
+app.delete('/entries/:id', async (req, res) => {
+    const { id } = req.params;
+    const entry = await Entry.findByIdAndDelete(id)
+    res.redirect('/entries')
 })
 
 app.listen(3000), () => {
--- seeds/entries.js
+++ seeds/entries.js
@@ -5,7 +5,7 @@ module.exports = [
             last_name: "Joyce",
             entries: [
                 {
-                    date_num: "20210312",
+                    date_num: "2021-03-12",
                     date: "March 12th, 2021",
                     subject: "Leadership Seminar",
                     body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique itaque quaerat, inventore nesciunt voluptatum ipsa voluptas illum? Quaerat similique, quis facilis quo cumque animi officiis molestiae omnis doloremque itaque veniam? Iusto voluptas quam explicabo excepturi quas, perspiciatis provident. Tenetur consequatur rerum explicabo error sunt! Et expedita id odio mollitia, exercitationem possimus, doloremque tempore libero, totam ullam nostrum qui eveniet molestias. Culpa, magni. Odio autem quas quis mollitia, ipsa provident. Aut sunt maiores dolorem, doloribus laborum esse obcaecati odit maxime tempora at sequi distinctio. Iste nisi non blanditiis aliquid voluptatibus quam? Qui quidem consequuntur aliquid nam suscipit ipsa labore eveniet dignissimos reprehenderit expedita, voluptatibus excepturi nesciunt architecto necessitatibus exercitationem ullam molestias ut dolore rem minus perspiciatis doloribus voluptatem? Nihil",
@@ -13,7 +13,7 @@ module.exports = [
                     tag: "Leadership"
                 },
                 {
-                    date_num: "20210314",
+                    date_num: "2021-03-14",
                     date: "March 14th, 2021",
                     subject: "Work Event",
                     body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique itaque quaerat, inventore nesciunt voluptatum ipsa voluptas illum? Quaerat similique, quis facilis quo cumque animi officiis molestiae omnis doloremque itaque veniam? Iusto voluptas quam explicabo excepturi quas, perspiciatis provident. Tenetur consequatur rerum explicabo error sunt! Et expedita id odio mollitia, exercitationem possimus, doloremque tempore libero, totam ullam nostrum qui eveniet molestias. Culpa, magni. Odio autem quas quis mollitia, ipsa provident. Aut sunt maiores dolorem, doloribus laborum esse obcaecati odit maxime tempora at sequi distinctio. Iste nisi non blanditiis aliquid voluptatibus quam? Qui quidem consequuntur aliquid nam suscipit ipsa labore eveniet dignissimos reprehenderit expedita, voluptatibus excepturi nesciunt architecto necessitatibus exercitationem ullam molestias ut dolore rem minus perspiciatis doloribus voluptatem? Nihil",
@@ -21,7 +21,7 @@ module.exports = [
                     tag: "Work"
                 },
                 {
-                    date_num: "20210313",
+                    date_num: "2021-03-13",
                     date: "March 13th, 2021",
                     subject: "Sermon",
                     body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique itaque quaerat, inventore nesciunt voluptatum ipsa voluptas illum? Quaerat similique, quis facilis quo cumque animi officiis molestiae omnis doloremque itaque veniam? Iusto voluptas quam explicabo excepturi quas, perspiciatis provident. Tenetur consequatur rerum explicabo error sunt! Et expedita id odio mollitia, exercitationem possimus, doloremque tempore libero, totam ullam nostrum qui eveniet molestias. Culpa, magni. Odio autem quas quis mollitia, ipsa provident. Aut sunt maiores dolorem, doloribus laborum esse obcaecati odit maxime tempora at sequi distinctio. Iste nisi non blanditiis aliquid voluptatibus quam? Qui quidem consequuntur aliquid nam suscipit ipsa labore eveniet dignissimos reprehenderit expedita, voluptatibus excepturi nesciunt architecto necessitatibus exercitationem ullam molestias ut dolore rem minus perspiciatis doloribus voluptatem? Nihil",
