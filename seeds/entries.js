33938a4c38d76afdcae846f70c44117a1d9b28db
Sean Joyce <joycesean702@gmail.com>
2021-06-27 18:34:18 +0100

    Styling

--- seeds/entries.js
+++ seeds/entries.js
@@ -2,25 +2,49 @@ module.exports = [
     {
         date_num: "2021-03-12",
         date: "Fri Mar 12 2021",
-        subject: "Leadership Seminar",
+        subject: "Lorem Ipsum Dolor",
         body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique itaque quaerat, inventore nesciunt voluptatum ipsa voluptas illum? Quaerat similique, quis facilis quo cumque animi officiis molestiae omnis doloremque itaque veniam? Iusto voluptas quam explicabo excepturi quas, perspiciatis provident. Tenetur consequatur rerum explicabo error sunt! Et expedita id odio mollitia, exercitationem possimus, doloremque tempore libero, totam ullam nostrum qui eveniet molestias. Culpa, magni. Odio autem quas quis mollitia, ipsa provident. Aut sunt maiores dolorem, doloribus laborum esse obcaecati odit maxime tempora at sequi distinctio. Iste nisi non blanditiis aliquid voluptatibus quam? Qui quidem consequuntur aliquid nam suscipit ipsa labore eveniet dignissimos reprehenderit expedita, voluptatibus excepturi nesciunt architecto necessitatibus exercitationem ullam molestias ut dolore rem minus perspiciatis doloribus voluptatem? Nihil",
-        rating: 8,
-        tag: "Leadership"
+        rating: 10,
+        tag: "Lorem"
     },
     {
         date_num: "2021-03-14",
         date: "Thu Mar 11 2021",
-        subject: "Work Event",
+        subject: "Lorem Ipsum Dolor",
         body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique itaque quaerat, inventore nesciunt voluptatum ipsa voluptas illum? Quaerat similique, quis facilis quo cumque animi officiis molestiae omnis doloremque itaque veniam? Iusto voluptas quam explicabo excepturi quas, perspiciatis provident. Tenetur consequatur rerum explicabo error sunt! Et expedita id odio mollitia, exercitationem possimus, doloremque tempore libero, totam ullam nostrum qui eveniet molestias. Culpa, magni. Odio autem quas quis mollitia, ipsa provident. Aut sunt maiores dolorem, doloribus laborum esse obcaecati odit maxime tempora at sequi distinctio. Iste nisi non blanditiis aliquid voluptatibus quam? Qui quidem consequuntur aliquid nam suscipit ipsa labore eveniet dignissimos reprehenderit expedita, voluptatibus excepturi nesciunt architecto necessitatibus exercitationem ullam molestias ut dolore rem minus perspiciatis doloribus voluptatem? Nihil",
-        rating: 9,
-        tag: "Work"
+        rating: 10,
+        tag: "Lorem"
     },
     {
         date_num: "2021-03-13",
-        date: "Sat Mar 13 2021",
-        subject: "Sermon",
+        date: "Wed Mar 13 2021",
+        subject: "Lorem Ipsum Dolor",
         body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique itaque quaerat, inventore nesciunt voluptatum ipsa voluptas illum? Quaerat similique, quis facilis quo cumque animi officiis molestiae omnis doloremque itaque veniam? Iusto voluptas quam explicabo excepturi quas, perspiciatis provident. Tenetur consequatur rerum explicabo error sunt! Et expedita id odio mollitia, exercitationem possimus, doloremque tempore libero, totam ullam nostrum qui eveniet molestias. Culpa, magni. Odio autem quas quis mollitia, ipsa provident. Aut sunt maiores dolorem, doloribus laborum esse obcaecati odit maxime tempora at sequi distinctio. Iste nisi non blanditiis aliquid voluptatibus quam? Qui quidem consequuntur aliquid nam suscipit ipsa labore eveniet dignissimos reprehenderit expedita, voluptatibus excepturi nesciunt architecto necessitatibus exercitationem ullam molestias ut dolore rem minus perspiciatis doloribus voluptatem? Nihil",
         rating: 10,
-        tag: "Faith"
+        tag: "Lorem"
+    },
+    {
+        date_num: "2021-03-15",
+        date: "Fri Mar 15 2021",
+        subject: "Lorem Ipsum Dolor",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique itaque quaerat, inventore nesciunt voluptatum ipsa voluptas illum? Quaerat similique, quis facilis quo cumque animi officiis molestiae omnis doloremque itaque veniam? Iusto voluptas quam explicabo excepturi quas, perspiciatis provident. Tenetur consequatur rerum explicabo error sunt! Et expedita id odio mollitia, exercitationem possimus, doloremque tempore libero, totam ullam nostrum qui eveniet molestias. Culpa, magni. Odio autem quas quis mollitia, ipsa provident. Aut sunt maiores dolorem, doloribus laborum esse obcaecati odit maxime tempora at sequi distinctio. Iste nisi non blanditiis aliquid voluptatibus quam? Qui quidem consequuntur aliquid nam suscipit ipsa labore eveniet dignissimos reprehenderit expedita, voluptatibus excepturi nesciunt architecto necessitatibus exercitationem ullam molestias ut dolore rem minus perspiciatis doloribus voluptatem? Nihil",
+        rating: 10,
+        tag: "Lorem"
+    },
+    {
+        date_num: "2021-03-16",
+        date: "Sat Mar 16 2021",
+        subject: "Lorem Ipsum Dolor",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique itaque quaerat, inventore nesciunt voluptatum ipsa voluptas illum? Quaerat similique, quis facilis quo cumque animi officiis molestiae omnis doloremque itaque veniam? Iusto voluptas quam explicabo excepturi quas, perspiciatis provident. Tenetur consequatur rerum explicabo error sunt! Et expedita id odio mollitia, exercitationem possimus, doloremque tempore libero, totam ullam nostrum qui eveniet molestias. Culpa, magni. Odio autem quas quis mollitia, ipsa provident. Aut sunt maiores dolorem, doloribus laborum esse obcaecati odit maxime tempora at sequi distinctio. Iste nisi non blanditiis aliquid voluptatibus quam? Qui quidem consequuntur aliquid nam suscipit ipsa labore eveniet dignissimos reprehenderit expedita, voluptatibus excepturi nesciunt architecto necessitatibus exercitationem ullam molestias ut dolore rem minus perspiciatis doloribus voluptatem? Nihil",
+        rating: 10,
+        tag: "Lorem"
+    },
+    {
+        date_num: "2021-03-17",
+        date: "Sun Mar 17 2021",
+        subject: "Lorem Ipsum Dolor",
+        body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique itaque quaerat, inventore nesciunt voluptatum ipsa voluptas illum? Quaerat similique, quis facilis quo cumque animi officiis molestiae omnis doloremque itaque veniam? Iusto voluptas quam explicabo excepturi quas, perspiciatis provident. Tenetur consequatur rerum explicabo error sunt! Et expedita id odio mollitia, exercitationem possimus, doloremque tempore libero, totam ullam nostrum qui eveniet molestias. Culpa, magni. Odio autem quas quis mollitia, ipsa provident. Aut sunt maiores dolorem, doloribus laborum esse obcaecati odit maxime tempora at sequi distinctio. Iste nisi non blanditiis aliquid voluptatibus quam? Qui quidem consequuntur aliquid nam suscipit ipsa labore eveniet dignissimos reprehenderit expedita, voluptatibus excepturi nesciunt architecto necessitatibus exercitationem ullam molestias ut dolore rem minus perspiciatis doloribus voluptatem? Nihil",
+        rating: 10,
+        tag: "Lorem"
     }
 ]
