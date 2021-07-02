ed42d3beb3c10db549237e98cb3fe8f01d31e4e1
Sean Joyce <joycesean702@gmail.com>
2021-07-02 07:35:29 +0100

    add styles

--- public/javascripts/active.js
+++ public/javascripts/active.js
@@ -2,9 +2,9 @@ const element1 = document.querySelector('.isActive1')
 const element2 = document.querySelector('.isActive2')
 const element3 = document.querySelector('.isActive3')
 
-if (element1) {
+if (element1 || element2 || element3) {
     element1.addEventListener("click", event => {
-        console.log('kaballas')
+        console.log(event)
         element1.classList.add('active')
         element2.classList.remove('active')
         element3.classList.remove('active')
