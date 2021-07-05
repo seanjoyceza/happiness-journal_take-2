c8e1b143d67123879244ed77557435d2a79f1198
Sean Joyce <joycesean702@gmail.com>
2021-07-05 07:51:48 +0100

    Add chart functionality

---
+++ public/javascripts/chart.js
@@ -0,0 +1,11 @@
+const displayFilter = document.querySelectorAll('.display_filter')
+const years = document.querySelector('#years')
+const chartRows = document.querySelectorAll(".chart_row")
+const chartBody = document.querySelector(".chart_body")
+
+for (i = 0; i < displayFilter.length; i++) {
+    displayFilter[i].addEventListener("click", (event) => {
+
+    })
+}
+
