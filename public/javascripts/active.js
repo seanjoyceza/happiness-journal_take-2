5032d9be145e07c4f94d26c46d4dfee3616efc7d
Sean Joyce <joycesean702@gmail.com>
2021-07-07 07:04:49 +0100

    Add styles

--- public/javascripts/active.js
+++ public/javascripts/active.js
@@ -2,9 +2,8 @@ const element1 = document.querySelector('.isActive1')
 const element2 = document.querySelector('.isActive2')
 const element3 = document.querySelector('.isActive3')
 
-if (element1 || element2 || element3) {
+try {
     element1.addEventListener("click", event => {
-        console.log(event)
         element1.classList.add('active')
         element2.classList.remove('active')
         element3.classList.remove('active')
@@ -21,6 +20,6 @@ if (element1 || element2 || element3) {
         element2.classList.remove('active')
         element3.classList.add('active')
     })
+} catch (e) {
 }
-
 
