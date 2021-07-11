c09e743d9530d9a5669e0a65270cae9378fd17f1
Sean Joyce <joycesean702@gmail.com>
2021-07-11 11:58:28 +0100

    Add functionality

--- schemas.js
+++ schemas.js
@@ -1,12 +1,37 @@
-const Joi = require('joi')
+const BaseJoi = require('joi')
+const sanitizeHtml = require('sanitize-html');
+
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': '{{#label}} must not include HTML!'
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [],
+                    allowedAttributes: {},
+                });
+                if (clean !== value) return helpers.error('string.escapeHTML', { value })
+                return clean;
+            }
+        }
+    }
+});
+
+const Joi = BaseJoi.extend(extension)
+
+
 // if (!req.body.entry) throw new ExpressError('Invalid Entry Data', 400) //if we have an error we throw an ExpressError, our catchAsync will then throw it to be caught by next
 module.exports.entrySchema = Joi.object({
     entry: Joi.object({
         date_num: Joi.required(),
-        date: Joi.string(),
-        subject: Joi.string().required(),
-        body: Joi.string().required(),
-        rating: Joi.string().required(),
-        tag: Joi.string().required()
+        date: Joi.string().escapeHTML(),
+        subject: Joi.string().required().escapeHTML(),
+        body: Joi.string().required().escapeHTML(),
+        rating: Joi.string().required().escapeHTML(),
+        tag: Joi.string().required().escapeHTML()
     }).required()
 })
